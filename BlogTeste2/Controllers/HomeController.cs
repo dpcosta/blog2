88c7d5ea3092f83915382caacccbcc7798ad24c1
dpcosta <dpcosta@gmail.com>
2017-03-09 15:16:53 -0300

    Exercício 1.2

--- BlogTeste2/Controllers/HomeController.cs
+++ BlogTeste2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogTeste2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,14 @@ namespace BlogTeste2.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var listaPosts = new List<Post>
+            {
+                new Post { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                new Post { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
+                new Post { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
+                new Post { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
+            };
+            ViewBag.Posts = listaPosts;
             return View();
         }
     }
