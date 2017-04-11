07b20c1daa08112098078cfef585145067d4db18
dpcosta <dpcosta@gmail.com>
2017-04-11 16:14:56 -0300

    Exercício 7.3

---
+++ BlogTeste2/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,96 @@
+﻿using BlogTeste2.Infra;
+using BlogTeste2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogTeste2.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        private BlogContext contexto;
+
+        public PostController()
+        {
+            this.contexto = new BlogContext();
+        }
+
+        // GET: Admin/Post
+        public ActionResult Index()
+        {
+            var lista = contexto.Posts.ToList();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Novo(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                contexto.Posts.Add(post);
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
+            } else
+            {
+                return View(post);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = contexto.Posts.Where(p => p.Id == id).FirstOrDefault();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                contexto.Entry(post).State = EntityState.Modified;
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
+            } else
+            {
+                return View(post);
+            }
+        }
+
+        public ActionResult Remover(int id)
+        {
+            var postAExcluir = contexto.Posts.Where(p => p.Id == id).FirstOrDefault();
+            contexto.Posts.Remove(postAExcluir);
+            contexto.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Publicar(int id)
+        {
+            var postAPublicar = contexto.Posts.Where(p => p.Id == id).FirstOrDefault();
+            postAPublicar.Publicado = true;
+            postAPublicar.DataPublicacao = DateTime.Now;
+            contexto.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult CategoriaAutocomplete(string term)
+        {
+            var model = contexto.Posts.ToList()
+                .Where(p => p.Categoria.Contains(term))
+                .Select(p => new { label = p.Categoria })
+                .Distinct();
+            return Json(model);
+        }
+    }
+}
--- BlogTeste2/Controllers/HomeController.cs
+++ BlogTeste2/Controllers/HomeController.cs
@@ -18,103 +18,18 @@ namespace BlogTeste2.Controllers
         {
             using (BlogContext contexto = new BlogContext())
             {
-                var lista = contexto.Posts.ToList();
+                var lista = contexto.Posts.Where(p=>p.Publicado).ToList();
                 return View(lista);
             }
         }
 
-        public ActionResult NovoPost()
-        {
-            var model = new Post();
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult AdicionaPost(Post post)
-        {
-             if (ModelState.IsValid)
-            {
-                using (BlogContext contexto = new BlogContext())
-                {
-                    contexto.Posts.Add(post);
-                    contexto.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            } else
-            {
-                return View("NovoPost", post);
-            }
-        }
-
-        public ActionResult Categoria([Bind(Prefix = "id")] string categoria)
+         public ActionResult Categoria([Bind(Prefix = "id")] string categoria)
         {
             using (BlogContext contexto = new BlogContext())
             {
                 //var lista = contexto.Posts.Where(post => post.Categoria.Contains(categoria)).ToList();
                 var lista = from p in contexto.Posts where p.Categoria.Contains(categoria) select p;
                 return View("Index", lista.ToList());
-            }
-        }
-
-        public ActionResult RemovePost(int id)
-        {
-            using (var contexto = new BlogContext())
-            {
-                var post = contexto.Posts.Find(id);
-                contexto.Posts.Remove(post);
-                contexto.SaveChanges();
-                return RedirectToAction("Index");
-            }
-        }
-
-        public ActionResult PublicaPost(int id)
-        {
-            using (var contexto = new BlogContext())
-            {
-                var post = contexto.Posts.Find(id);
-                post.Publicado = true;
-                post.DataPublicacao = DateTime.Now;
-                contexto.SaveChanges();
-                return RedirectToAction("Index");
-            }
-        }
-
-        public ActionResult Visualiza(int id)
-        {
-            using (var contexto = new BlogContext())
-            {
-                var post = contexto.Posts.Find(id);
-                 return View(post);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult EditaPost(Post post)
-        {
-            if (ModelState.IsValid)
-            {
-                using (var contexto = new BlogContext())
-                {
-                    contexto.Entry(post).State = EntityState.Modified;
-                    contexto.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            } else
-            {
-                return View("Visualiza", post);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult CategoriaAutocomplete(string term)
-        {
-            using (var contexto = new BlogContext())
-            {
-                var model = contexto.Posts.ToList()
-                    .Where(p => p.Categoria.Contains(term))
-                    .Select(p => new { label = p.Categoria })
-                    .Distinct();
-                return Json(model);
             }
         }
 
